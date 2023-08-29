ceb0f522e399ceb1bfffda8c5f5dabd24ad50ca7
Raffaele Ianniello <134380099+RaffaeleIanniello@users.noreply.github.com>
2023-08-29 15:54:19 +0200

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Prodotto
+    {
+        public string Nome { get; set; }
+        public double Prezzo { get; set;}
+
+        public double Quantità { get; set;} 
+    }
+   
+   
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci il nome del prodotto:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Inserisci il prezzo del prodotto:");
+            double prezzo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Inserisci la quantità del prodotto:");
+            double quantità = Convert.ToDouble(Console.ReadLine());
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
